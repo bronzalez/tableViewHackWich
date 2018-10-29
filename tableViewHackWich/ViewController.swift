0a4be4251098076a62347b7b2ea7659ec512370f
BRIAN GONZALEZ <rl239067@rlas-macair-38.local>
2018-10-29 12:09:44 -0500

    Initial Commit

---
+++ tableViewHackWich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableViewHackWich
+//
+//  Created by BRIAN GONZALEZ on 10/29/18.
+//  Copyright © 2018 BRIAN GONZALEZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
